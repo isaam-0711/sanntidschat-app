e39359b31acff78111c9d2d9f7f83f51610fe470
Egil Hagen <egilhagen@Egils-MacBook-Air.local>
2025-04-07 19:43:38 +0200

    setup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   experimental: {
-    appDir: true,
+    // Remove the appDir key if it's not needed
+    // appDir: true,
   },
 };
-
-module.exports = nextConfig;
